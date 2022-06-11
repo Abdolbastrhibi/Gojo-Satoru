3bb5130ed00165c377f89b7cfa1dd1b26cdfc16a
Abdolbastrhibi <106561119+Abdolbastrhibi@users.noreply.github.com>
2022-06-11 13:12:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['212603582753']
+global.premium = ['212603582753']
+global.ownernomer = '212603582753'
+global.ownername = 'Star '
+global.botname = 'Stat'
+global.footer = '©Star.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'star'
+global.author = 'Starr'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','😭','😁','🤨']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
